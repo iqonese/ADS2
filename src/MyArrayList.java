a896fe81a32e01ee3d1c04ff710c3ec32f9a22f1
iqonese <iqonese@gmail.com>
2023-04-21 18:51:02 +0600

    Revert "MyArrayList added lastIndexOf method"
    This reverts commit 21f5e2cb34c3a495205bd7256224743628b649df.

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -82,15 +82,7 @@ public class MyArrayList<T> implements MyList {
 
 
     public int lastIndexOf(Object o) {
-        // initialize it with -1 to prevent errors
-        // each time we meet the desired outcome, we keep updating temp and in the end return it
-        int temp = -1;
-        for (int i = 0; i < size; i++) {
-            if (arr[i] == o) {
-                temp = i;
-            }
-        }
-        return temp;
+        return 0;
     }
 
 
